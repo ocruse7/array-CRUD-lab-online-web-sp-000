88d461db1b8714236c7e72969835095c9547ff66
Owen Cruse <ocruse7@gmail.com>
2020-11-26 00:20:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,14 +22,14 @@ def remove_element_from_start_of_array(array)
   array.shift
 end
 
-def retreive_element_from_index(array, index_number)
+def retrieve_element_from_index(array, index_number)
   array[index_number]
 end
 
-def retreive_first_element_from_array(array)
+def retrieve_first_element_from_array(array)
   array[0]
 end
 
-def retreive_last_element_from_array(array)
+def retrieve_last_element_from_array(array)
   array[-1]
 end
